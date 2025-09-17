78b84c955b2cb71e1b0e879e945e19599cb62012
Mohini-2005 <mohinigujjala27@gmail.com>
2025-09-17 04:37:49 +0000

    17/09/25

---
+++ switch1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+
+    int daynumber =2;
+    switch (daynumber)
+    {
+    case 1:
+    printf("Monday");
+    break;
+    case 2:
+    printf("Tuesday");
+    break;
+    case 3:
+    printf("wednesday");
+    break;
+    case 4:
+    printf("Thursday");
+    break;
+    case 5:
+    printf("Friday");
+    break;
+    case 6:
+    printf("Saturday");
+    break;
+    case 7:
+    printf("Sunday");
+    break;efault:
+        printf("Invalid day");
+    }
+}
