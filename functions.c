a8894fd03d1f9764f00e60271e58266dbd585375
Mohini-2005 <mohinigujjala27@gmail.com>
2025-09-30 03:32:51 +0000

    30/9/25

---
+++ functions.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+// Function Declaration -  Inform
+void greet();
+void add();
+void sub();
+void multi();
+
+int main(){
+    // Function Call - Use
+// greet();
+// greet();
+// greet();
+//add(1,1);
+//add(2,2);
+//add(3,3);
+//add(31,32);
+//sub(35,12);
+//sub(67,34);
+multi(2,4);
+multi(4,5);
+}
+
+// Function DEFINITION - Create
+void greet(){
+    printf("Good Morning. Mohini");
+}
+
+
+
+void add(int a,int b){
+   
+    printf("%d\n",a+b);
+}
+void sub(int a,int b){
+   
+    printf("%d\n",a-b);
+}
+void multi(int a,int b){
+   
+    printf("%d\n",a*b);
+}
