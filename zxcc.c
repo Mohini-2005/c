6fe8249738ab48e0c93f214fdeb35b1bea163750
Mohini-2005 <mohinigujjala27@gmail.com>
2025-10-06 03:46:48 +0000

    fact.c

---
+++ zxcc.c
@@ -0,0 +1,13 @@
+ #include<stdio.h>  
+ void change(int*num) {    
+     printf("first num=%d \n",num);    
+    (*num) += 100;     
+     printf("second num=%d \n", num);    
+ }    
+int main() {    
+         int x=100;    
+     printf("frist call x=%d \n", x);    
+   change(&x);
+printf("second call x=%d \n", x);    
+}
+    
