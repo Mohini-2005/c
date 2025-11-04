99bcdde996dce96aa1ed77a3118e86d8609ab19d
Mohini-2005 <mohinigujjala27@gmail.com>
2025-11-04 03:45:06 +0000

    3/11

---
+++ stringsExp.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    char name[30]={'j','h','o','n','d','o','e','0'};
+    char address[50]= "Pune";
+
+    printf("Enter your name");
+    scanf("%s",name);
+    printf("Name:%s\n",name);
+    printf("Address:%s\n",address);
+
+}
